34e26f402cd3aaca8d1b5501dc7c2f62985aa4b3
Robert Bender <robertbender@destynys-ipad.musd.mpsaz.org>
2019-05-24 11:24:02 -0700

    Installed Vuetify, fontAwesome, updated main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Vuetify from 'vuetify'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee, faChild, faCircle, faArchive, faCode, faIdBadge } from '@fortawesome/free-solid-svg-icons'
+import { faComment } from '@fortawesome/free-regular-svg-icons'
+import { faTwitter, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText } from '@fortawesome/vue-fontawesome'
+import 'vuetify/dist/vuetify.min.css'
+
+library.add(
+  faCoffee,
+  faChild,
+  faCircle,
+  faArchive,
+  faComment,
+  faTwitter,
+  faGithub,
+  faLinkedin,
+  faCode,
+  faIdBadge
+)
+
+Vue.use(Vuetify)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.component('font-awesome-layers', FontAwesomeLayers)
+Vue.component('font-awesome-layers-text', FontAwesomeLayersText)
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,42 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div id="app">
+    <main class="w-100 min-vh-100 bg-gray8 white sans-serif pa6 flex flex-column justify-center items-center">
+      <div class="mw8 center overflow-hidden">
+        <h2 class="tc ttu tracked3 b f2 mt0 mb2 teal0 o-30">vue-fontawesome</h2>
+
+        <ul class="list ma0 pa0 flex flex-row flex-wrap teal4">
+          <li class="pv3 ph2 ma0 link grow">
+            <font-awesome-icon icon="coffee" size="4x" />
+          </li>
+          <li class="pv3 ph2 ma0 link grow">
+            <font-awesome-icon :icon="['fas', 'coffee']" flip="horizontal" size="4x" />
+          </li>
+          <li class="pv3 ph2 ma0 link grow">
+            <font-awesome-icon :icon="['far', 'comment']" size="4x" />
+          </li>
+          <li class="pv3 ph2 ma0 link grow">
+            <font-awesome-icon icon="child" transform="shrink-4 down-2 right-2" :mask="['fas', 'circle']" size="4x" />
+          </li>
+          <li class="pv3 ph2 ma0 link grow">
+            <font-awesome-icon :icon="['fab', 'twitter']" size="4x" />
+          </li>
+          <li class="pv3 ph2 ma0 link grow">
+            <font-awesome-icon :icon="queen" size="4x" />
+          </li>
+          <li class="pv3 ph2 ma0 link grow">
+            <font-awesome-layers full-width class="fa-4x">
+              <font-awesome-icon :icon="['fas', 'archive']"/>
+              <font-awesome-layers-text class="red8" transform="down-3 shrink-14" value="SECRET" />
+            </font-awesome-layers>
+          </li>
+        </ul>
+      </div>
+    </main>
+  </div>
+    
+  </div>
   </div>
 </template>
 
@@ -16,3 +51,27 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+
+body {
+  margin: 0;
+  padding: 0;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
+
