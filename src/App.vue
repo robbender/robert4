cffd0c7abfe574b5f53dc4c841e40d2d8c5a15fe
Robert Bender <robertbender@destynys-ipad.musd.mpsaz.org>
2019-05-28 13:08:01 -0700

    Added vee-validate and vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,126 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
+<div id="app">
+    <div id="nav">
+        <template>
+            <v-navigation-drawer fixed v-model="drawerRight" right clipped app>
+                <v-list dense>
+                    <v-list-tile @click.stop="right = !right">
+                        <v-list-tile-action>
+                            <v-icon>exit_to_app</v-icon>
+                        </v-list-tile-action>
+                        <v-list-tile-content>
+                            <v-list-tile-title>Open Temporary Drawer</v-list-tile-title>
+                        </v-list-tile-content>
+                    </v-list-tile>
+                </v-list>
+            </v-navigation-drawer>
+            <v-navigation-drawer v-model="drawer" fixed app>
+                <v-list dense>
+                    <v-list-tile @click="">
+                        <v-list-tile-action>
+                            <v-icon>home</v-icon>
+                        </v-list-tile-action>
+                        <v-list-tile-content>
+                          <router-link to="/">Home</router-link>
+                            <!-- <v-list-tile-title>Home</v-list-tile-title> -->
+                        </v-list-tile-content>
+                    </v-list-tile>
+                    <v-list-tile @click="">
+                        <v-list-tile-action>
+                            <v-icon>person</v-icon>
+                            </v-list-tile-action>
 
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+                            <!-- <v-list-tile-content>
+                                <router-link to="/about">About</router-link>
+                            </v-list-tile-content> -->
+
+                        </v-list-tile-action>
+                        <v-list-tile-content>
+                          <router-link to="/about">About</router-link>
+                            <!-- <v-list-tile-title>About</v-list-tile-title> -->
+                        </v-list-tile-content>
+                    </v-list-tile>
+
+                    <v-list-tile @click="">
+                        <v-list-tile-action>
+                            <v-icon>contact_mail</v-icon>
+                        </v-list-tile-action>
+                        <v-list-tile-content>
+                          <router-link to="/contact">Contact</router-link>
+                            <!-- <v-list-tile-title>Contact</v-list-tile-title> -->
+                        </v-list-tile-content>
+                    </v-list-tile>
+                </v-list>
+            </v-navigation-drawer>
+            <v-toolbar app color="#def2f1">
+                <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+                <v-toolbar-title class="headline text-uppercase">
+                    <span>Robert</span>
+                    <span class="font-weight-light">Bender</span>
+                </v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-btn flat href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
+                    <span class="mr-2">Contact Me</span>
+                </v-btn>
+                <v-btn icon>
+                    <!-- <v-toolbar-side-icon @click.stop="drawerRight = !drawerRight"></v-toolbar-side-icon> -->
+                    <v-icon @click.stop="drawerRight = !drawerRight">more_vert</v-icon>
+                </v-btn>
+            </v-toolbar>
+            <v-content>
+                <Landing />
+            </v-content>
+            </v-app>
+        </template>
+
+    </div>
+    <router-view />
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
+// import Landing from './components/Landing'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
-    return {
-      //
+    name: 'App',
+    components: {
+    },
+    data: () => ({
+        drawer: false,
+        drawerRight: false,
+        right: null,
+        left: null
+    }),
+    props: {
+        source: String
     }
-  }
 }
 </script>
+
+<style scoped>
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+body {
+    margin: 0;
+    padding: 0;
+}
+h3 {
+    margin: 40px 0 0;
+}
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+a {
+    color: #42b983;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,76 @@
 <template>
-  <HelloWorld />
+  <v-container>
+    <v-layout
+      text-xs-center
+      wrap
+    >
+      <v-flex xs12>
+        <main class="w-100 min-vh-100 sans-serif pa6 flex flex-column justify-center items-center">
+      <div class="mw8 center overflow-hidden">
+        <h2 class="tc ttu tracked3 b f2 mt0 mb2 o-1">About Robert</h2>
+
+        <ul class="list ma0 pa0 flex flex-row flex-wrap teal4">
+          <li class="pv3 ph2 ma0 link grow">
+            <a href=""><font-awesome-icon :icon="['fas', 'code']" transform="shrink-4 down-0 left-0" :mask="['fas', 'circle']" size="4x" @click="code" /></a>
+          </li>
+          
+          <li class="pv3 ph2 ma0 link grow">
+            <a href="" target="_blank"><font-awesome-icon :icon="['fas', 'id-badge']" size="4x" /></a>
+          </li>
+          <li class="pv3 ph2 ma0 link grow">
+            <a href="https://github.com/robbender" target="_blank"><font-awesome-icon :icon="['fab', 'github']" size="4x" /></a>
+          </li>
+          <li class="pv3 ph2 ma0 link grow">
+            <a href="https://www.linkedin.com/in/robert-bender-b152257/" target="_blank"><font-awesome-icon :icon="['fab', 'linkedin']" size="4x" /></a>
+          </li>
+        </ul>
+      </div>
+    </main>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    components: {
-      HelloWorld
-    }
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
   }
+}
 </script>
+
+<style scoped>
+body {
+  margin: 0;
+  padding: 0;
+  /* background-color: #3AAFA9; */
+}
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    /* color: #17252A; */
+    background-color: #3AAFA9;
+}
+
+#17252A
+
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #17252A;
+}
+</style>
